d2a9427cebd0842244d51e2c1a964a0e837bffe0
Patricia Palacios <patricia.palaciosg@alumnos.upm.es>
2015-05-03 12:23:55 +0200

    CSS adaptable

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,12 @@ var quizController = require('../controllers/quiz_controller');
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
-});
-
+}); 
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
+
+router.get('/author', function(req, res, next) {
+	res.render('author');
+});
 
 module.exports = router;

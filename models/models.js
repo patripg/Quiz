cc6d34f904210f446468db12f2714f985fcd55a1
Patricia Palacios <patricia.palaciosg@alumnos.upm.es>
2015-05-10 12:29:02 +0200

    prueba3

--- models/models.js
+++ models/models.js
@@ -33,14 +33,17 @@ var Quiz = sequelize.import(quiz_path);
 exports.Quiz = Quiz; // exportar definicion de tabla Quiz
 
 // sequelize.sync() crea e inicializa tabla de preguntas en DB
-sequelize.sync().success(function() {
+sequelize.sync().then(function() {
 	// success(..) ejecuta el manejador una vez creada la tabla
-	Quiz.count().success(function(count){
+	Quiz.count().then(function (count){
 		if(count === 0) {	// la tabla se inicializa solo si esta vacia
 			Quiz.create({ pregunta: 'Capital de Italia',
 				respuesta: 'Roma'
+			});
+			Quiz.create({ pregunta: 'Capital de Portugal',
+				respuesta: 'Lisboa'
 			})
-			.success(function(){console.log('Base de datos inicializada')});
+			.then(function(){console.log('Base de datos inicializada')});
 		};
 	});
 });
